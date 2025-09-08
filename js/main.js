4fa25dfef79ed7eb2d995248f55e1ec99d45808b
Martijn Tas <martijn.tas5@gmail.com>
2025-09-08 14:04:28 +0200

    add video player

--- js/main.js
+++ js/main.js
@@ -2,4 +2,21 @@ window.addEventListener('load', init)
 
 function init() {
     console.log('Hello, world!')
+    togglePlayPause();
 }
+
+function togglePlayPause() {
+    const videos = document.querySelectorAll('video');
+
+    for (const video of videos) {
+        video.addEventListener('click', function () {
+            console.log('clicked');
+            if (video.paused) {
+                video.play();
+            } else {
+                video.pause();
+            }
+        });
+    }
+}
+
