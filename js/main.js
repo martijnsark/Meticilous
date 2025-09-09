c7b67884e327457eecc99b76d69ded6f0460342c
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-09-09 12:42:55 +0200

    added local storage to likes

--- js/main.js
+++ js/main.js
@@ -26,22 +26,40 @@ function handleLikes() {
 
     likeButtons.forEach(button => {
         const icon = button.querySelector('.material-icons');
-        // Check if the button is a like button
+        // Check if the button clicked is a like button
         if (icon && (icon.textContent.trim() === 'favorite_border' || icon.textContent.trim() === 'favorite')) {
+            const videoElement = button.closest('.video').querySelector('.video__player');
+            // video source as unique key for localstorage
+            const videoSrc = videoElement.src;
+            // Get the paragraph element that displays the like count
+            const likeCountElement = button.querySelector('p');
+            // Get the current like count value from the element
+            let likeCount = parseInt(likeCountElement.textContent);
+
+            // Load liked state from localStorage
+            if (localStorage.getItem(videoSrc) === 'true') {
+                icon.textContent = 'favorite';
+                // Increment the displayed like count if already liked
+                likeCountElement.textContent = likeCount + 1;
+            }
+
             button.addEventListener('click', () => {
-                const likeCountElement = button.querySelector('p');
-                let likeCount = parseInt(likeCountElement.textContent);
+                // double check count in case of changes
+                let currentLikeCount = parseInt(likeCountElement.textContent);
 
                 if (icon.textContent.trim() === 'favorite_border') {
                     icon.textContent = 'favorite';
-                    likeCount++;
+                    //add like to count
+                    currentLikeCount++;
+                    localStorage.setItem(videoSrc, 'true');
                 } else {
                     icon.textContent = 'favorite_border';
-                    likeCount--;
+                    //remove like from count
+                    currentLikeCount--;
+                    localStorage.setItem(videoSrc, 'false');
                 }
-                likeCountElement.textContent = likeCount;
+                likeCountElement.textContent = currentLikeCount;
             });
         }
     });
 }
-
