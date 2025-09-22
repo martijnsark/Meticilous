39fae4df73a2e37d7bcc3dfc5f0f90714924d288
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-09-22 13:00:48 +0200

    custom pop UI for location

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
 
 <body>
 
-<?php
-// list with all video's
-$videos = [
+    <?php
+    // list with all video's
+    $videos = [
         "video1.mp4",
         "video2.mp4",
         "video3.mp4",
@@ -60,12 +60,12 @@ $videos = [
         "video40.mp4",
         "video41.mp4",
         "video42.mp4",
-];
+    ];
 
-shuffle($videos);
+    shuffle($videos);
 
 
-?>
+    ?>
     <header class="header">
         <a href="index.php">
             <img src="images/logo.png" alt="Meticulous Logo" class="logo">
@@ -84,40 +84,46 @@ shuffle($videos);
         </div>
     </dialog>
 
-<div class="app__videos">
-    <?php foreach ($videos as $index => $video): ?>
-        <div class="video" id="video-<?php echo $index+1; ?>">
-            <video class="video__player"
-                   loop
-                   controls
-                   src="https://github.com/martijnsark/Meticilous/raw/refs/heads/main/videos/<?php echo $video; ?>">
-            </video>
+    <div class="app__videos">
+        <?php foreach ($videos as $index => $video): ?>
+            <div class="video" id="video-<?php echo $index + 1; ?>">
+                <video class="video__player"
+                    loop
+                    controls
+                    src="https://github.com/martijnsark/Meticilous/raw/refs/heads/main/videos/<?php echo $video; ?>">
+                </video>
 
-            <!-- sidebar -->
-            <div class="videoSidebar">
-                <div class="videoSidebar__button">
-                    <span class="material-icons"> favorite_border </span>
-                    <p>12</p>
-                </div>
+                <!-- sidebar -->
+                <div class="videoSidebar">
+                    <div class="videoSidebar__button">
+                        <span class="material-icons"> favorite_border </span>
+                        <p>12</p>
+                    </div>
 
-                <div class="videoSidebar__button">
-                    <span class="material-icons"> message </span>
-                    <p>23</p>
-                </div>
+                    <div class="videoSidebar__button">
+                        <span class="material-icons"> message </span>
+                        <p>23</p>
+                    </div>
 
-                <div class="videoSidebar__button save-button">
-                    <span class="material-icons"> bookmark_border </span>
-                    <p>Save</p>
-                </div>
+                    <div class="videoSidebar__button save-button">
+                        <span class="material-icons"> bookmark_border </span>
+                        <p>Save</p>
+                    </div>
 
-                <div class="videoSidebar__button">
-                    <span class="material-icons"> share </span>
-                    <p>75</p>
+                    <div class="videoSidebar__button">
+                        <span class="material-icons"> share </span>
+                        <p>75</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    <?php endforeach; ?>
-</div>
+        <?php endforeach; ?>
+    </div>
+
+    <div id="location-popup" class="hidden">
+        <p id="location-text"></p>
+        <button id="close-location-popup">CLOSE</button>
+    </div>
+
 </body>
 
 </html>
