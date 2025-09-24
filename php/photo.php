cb64b7a73c1f2e228f5c6e0ee42f7a4fbc5a9fcf
qixel2003 <66803720+qixel2003@users.noreply.github.com>
2025-09-24 10:55:24 +0200

    photo test

---
+++ php/photo.php
@@ -0,0 +1,21 @@
+<?php
+// eventueel je eigen PHP code bovenin
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Selfie maken</title>
+    <script src="../js/camera.js"></script>
+</head>
+<body>
+
+<h1>Selfie maken</h1>
+
+<video id="video" autoplay playsinline></video>
+<button onclick="maakSelfie()">📸 Foto maken</button>
+<canvas id="canvas" style="display:none;"></canvas>
+<img id="snapshot" alt="Jouw foto" />
+
+</body>
+</html>
